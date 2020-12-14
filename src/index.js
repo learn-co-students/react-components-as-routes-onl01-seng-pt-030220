8b2b2bf8e9aae5d02d2a2ebaa417b0a7f2b3656c
kelly_maass <kmaass77627@gmail.com>
2020-12-13 19:53:39 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,45 @@ import ReactDOM from 'react-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>Home Page</h1>
     </div>
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const Movie = () => {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+    </div>
+  );
+};
+
+const Director = () => {
+  return (
+    <div>
+      <h1>Directors Page</h1>
+    </div>
+  );
+};
+
+const Actor = () => {
+  return (
+    <div>
+      <h1>Actors Page</h1>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  
+  <Router>
+    <div>
+      <Navbar/>
+      <Route path="/" component={Home} />
+      <Route exact path="/movies" component={Movie} />
+      <Route exact path="/directors" component={Director} />
+      <Route exact path="/actors" component={Actor} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
